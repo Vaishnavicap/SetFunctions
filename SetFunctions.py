8881e8623ccaea639c7e59e0cfce31135afdb2db
Vaishnavicap <90391261+Vaishnavicap@users.noreply.github.com>
2021-11-27 15:28:08 +0530

    Add files via upload

---
+++ SetFunctions.py
@@ -0,0 +1,8 @@
+'''Here we have to define two set variables and we have to perform different set operations:
+union, intersection, difference and symmetric difference.'''
+A = {0,1,2,3,4,5,6,7,8} # Set A
+B = {2,4,6} #Set B
+print("Union of A and B is ",A.union(B))
+print("Intersection of A and B is ",A.intersection(B))
+print("Difference of A and B is ",A.difference(B))
+print("Symmetric difference of A and B is ",A.symmetric_difference(B))
